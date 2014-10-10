ea103fdaca7d6d8cb7d72668d5f188b5ee2ffecc
Kajna <milos@warpmail.net>
2014-10-10 03:57:06 +0200

    Deprecated baseUrl() function.

--- tests/Util/UtilTest.php
+++ tests/Util/UtilTest.php
@@ -13,7 +13,6 @@ class UtilTest extends PHPUnit_Framework_TestCase
 
     	// Test assets get (remember PUBLIC folder is set to 'public')
     	$this->assertEquals(Util::base('foo'), 'http://localhost/www/foo');
-        $this->assertEquals(Util::baseUrl('foo'), 'http://localhost/www/foo');
     	$this->assertEquals(Util::css('foo.css'), 'http://localhost/www/public/css/foo.css');
     	$this->assertEquals(Util::js('foo.js'), 'http://localhost/www/public/js/foo.js');
     	$this->assertEquals(Util::img('foo.png'), 'http://localhost/www/public/images/foo.png');
