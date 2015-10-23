09a33e77784e77ce3e3393f5ae8aca25e5071601
Kajna <milos@caenazzo.com>
2015-10-23 18:12:48 +0200

    2.1.0 RC1

--- App/routes.php
+++ App/routes.php
@@ -4,7 +4,7 @@
 | Application Routes
 |--------------------------------------------------------------------------
 | All application routes are defined here. 
-| (For controllers prefix App\Controllers is added automaticly and it isn't needed here)
+| (For controllers prefix const is CONTROLLERS_ROOT if defined otherwise App\Controllers\E is added and isn't needed here)
 */
 $route->get('', 'ExampleController', 'indexAction');
 $route->get('contact', 'ExampleController', 'contactAction');
